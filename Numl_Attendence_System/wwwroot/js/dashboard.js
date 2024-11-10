66ac75aa1f8d53eb289ff2ae9c2a84de35844cb5
Huzaifa-Awan <huzaifa_internee@dpslahore.edu.pk>
2024-11-10 23:30:00 +0500

    A lot of Changes

---
+++ Numl_Attendence_System/wwwroot/js/dashboard.js
@@ -0,0 +1,29 @@
+﻿function checkDropdowns() {
+    var session = $("#sessionDropdown").val();
+    var semester = $("#semesterDropdown").val();
+    var section = $("#sectionDropdown").val();
+    var subject = $("#subjectDropdown").val();
+    var slot = $("#slotDropdown").val();
+    if (semester == ("1")) {
+        if (session !== null && semester !== null && section !== null && subject !== null && slot !== null) {
+            $("#submitButton").prop('disabled', false);
+        }
+        else {
+            $("#submitButton").prop('disabled', true);
+        }
+    }
+    else {
+        if (session !== null && semester !== null && subject !== null && slot !== null) {
+            $("#submitButton").prop('disabled', false);
+        }
+        else {
+            $("#submitButton").prop('disabled', true);
+        }
+    }
+}
+
+$("#submitButton").on('click', function () {
+    $("slotDropdown").val("2");
+    //
+});
+
